f7d863f17859e20bdd50b312db15fac040b17da2
MarshallJoseph <mrmarshalljoseph@gmail.com>
2021-04-17 15:45:02 -0400

    major updates to UI, search functionality fixed

--- app/src/main/java/ca/marshall/xlightstools/ARPropsActivity.java
+++ app/src/main/java/ca/marshall/xlightstools/ARPropsActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.media.CamcorderProfile;
 import android.net.Uri;
 import android.os.Bundle;
@@ -35,9 +34,17 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
                             treeRenderable,
                             matrixRenderable,
                             starRenderable,
-                            candycaneRenderable;
+                            candycaneRenderable,
+                            circleboarderRenderable,
+                            squareboarderRenderable,
+                            megatreeRenderable,
+                            minitreeRenderable,
+                            snowflakeRenderable,
+                            sphereRenderable,
+                            presentRenderable,
+                            pipeRenderable;
 
-    ImageView arch, tree, matrix, star, candycane;
+    ImageView arch, tree, matrix, star, candycane, circleboarder, squareboarder, megatree, minitree, snowflake, sphere, present, pipe;
 
     View[] arrayView;
 
@@ -128,10 +135,18 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         arch = findViewById(R.id.arch);
-        tree = findViewById(R.id.tree);
+        tree = findViewById(R.id.spiraltree);
         matrix = findViewById(R.id.matrix);
         star = findViewById(R.id.star);
         candycane = findViewById(R.id.candycane);
+        circleboarder = findViewById(R.id.circleboarder);
+        squareboarder = findViewById(R.id.squareboarder);
+        megatree = findViewById(R.id.megatree);
+        minitree = findViewById(R.id.minitree);
+        snowflake = findViewById(R.id.snowflake);
+        sphere = findViewById(R.id.sphere);
+        present = findViewById(R.id.present);
+        pipe = findViewById(R.id.pipe);
 
         setArrayView();
         setClickListener();
@@ -244,7 +259,7 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
                 .thenAccept(renderable -> archRenderable = renderable)
                 .exceptionally(
                         throwable -> {
-                            Toast.makeText(this, "Unable to load arch model", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
                             return null;
                         }
                 );
@@ -254,7 +269,7 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
                 .thenAccept(renderable -> treeRenderable = renderable)
                 .exceptionally(
                         throwable -> {
-                            Toast.makeText(this, "Unable to load spiral tree model", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
                             return null;
                         }
                 );
@@ -264,7 +279,7 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
                 .thenAccept(renderable -> matrixRenderable = renderable)
                 .exceptionally(
                         throwable -> {
-                            Toast.makeText(this, "Unable to load matrix model", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
                             return null;
                         }
                 );
@@ -274,17 +289,97 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
                 .thenAccept(renderable -> starRenderable = renderable)
                 .exceptionally(
                         throwable -> {
-                            Toast.makeText(this, "Unable to load star model", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
                             return null;
                         }
                 );
         ModelRenderable.builder()
-                .setSource(this, Uri.parse("CandyCane.sfb"))
+                .setSource(this, Uri.parse("Candy Cane.sfb"))
                 .build()
                 .thenAccept(renderable -> candycaneRenderable = renderable)
                 .exceptionally(
                         throwable -> {
-                            Toast.makeText(this, "Unable to load candy cane model", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Circle Boarder.sfb"))
+                .build()
+                .thenAccept(renderable -> circleboarderRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Square Border.sfb"))
+                .build()
+                .thenAccept(renderable -> squareboarderRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Mega Tree.sfb"))
+                .build()
+                .thenAccept(renderable -> megatreeRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("MiniTree.sfb"))
+                .build()
+                .thenAccept(renderable -> minitreeRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Snow Flake.sfb"))
+                .build()
+                .thenAccept(renderable -> snowflakeRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Sphere.sfb"))
+                .build()
+                .thenAccept(renderable -> sphereRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Present.sfb"))
+                .build()
+                .thenAccept(renderable -> presentRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("Pipe.sfb"))
+                .build()
+                .thenAccept(renderable -> pipeRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load model", Toast.LENGTH_SHORT).show();
                             return null;
                         }
                 );
@@ -321,6 +416,54 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
             node.setRenderable(candycaneRenderable);
             node.select();
             numObjects++;
+        } else if (selected == 6) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(circleboarderRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 7) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(squareboarderRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 8) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(megatreeRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 9) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(minitreeRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 10) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(snowflakeRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 11) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(sphereRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 12) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(presentRenderable);
+            node.select();
+            numObjects++;
+        } else if (selected == 13) {
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(pipeRenderable);
+            node.select();
+            numObjects++;
         }
     }
 
@@ -332,7 +475,7 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
 
     private void setArrayView() {
         arrayView = new View[] {
-                arch, tree, matrix, star, candycane
+                arch, tree, matrix, star, candycane, circleboarder, squareboarder, megatree, minitree, snowflake, sphere, present, pipe
         };
     }
 
@@ -340,8 +483,7 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view) {
         if (view.getId() == R.id.arch) {
             selected = 1;
-            setBackground(view.getId());
-        } else if (view.getId() == R.id.tree) {
+        } else if (view.getId() == R.id.spiraltree) {
             selected = 2;
         } else if (view.getId() == R.id.matrix) {
             selected = 3;
@@ -349,15 +491,22 @@ public class ARPropsActivity extends AppCompatActivity implements View.OnClickLi
             selected = 4;
         } else if (view.getId() == R.id.candycane) {
             selected = 5;
-        }
-    }
-
-    private void setBackground(int id) {
-        for (int i = 0; i < arrayView.length; i++) {
-            if (arrayView[i].getId() == id)
-                arrayView[i].setBackgroundColor(Color.parseColor("#80333639")); // set background for selected object
-            else
-                arrayView[i].setBackgroundColor(Color.TRANSPARENT);
+        } else if (view.getId() == R.id.circleboarder) {
+            selected = 6;
+        } else if (view.getId() == R.id.squareboarder) {
+            selected = 7;
+        } else if (view.getId() == R.id.megatree) {
+            selected = 8;
+        } else if (view.getId() == R.id.minitree) {
+            selected = 9;
+        } else if (view.getId() == R.id.snowflake) {
+            selected = 10;
+        } else if (view.getId() == R.id.sphere) {
+            selected = 11;
+        } else if (view.getId() == R.id.present) {
+            selected = 12;
+        } else if (view.getId() == R.id.pipe) {
+            selected = 13;
         }
     }
 
--- app/src/main/java/ca/marshall/xlightstools/SearchActivity.java
+++ app/src/main/java/ca/marshall/xlightstools/SearchActivity.java
@@ -1,10 +1,13 @@
 package ca.marshall.xlightstools;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -82,35 +85,49 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
-        btn = (Button) this.findViewById(R.id.goBtn);
-        sv = this.findViewById(R.id.searchView);
+        adapter = new WebsiteAdapter(this, websiteList);
+        lv = findViewById(R.id.listView);
+        lv.setAdapter(adapter);
 
+        btn = (Button) findViewById(R.id.goBtn);
+
+        sv = findViewById(R.id.searchView);
+        sv.setFocusable(false); // make the search view NOT automatically clicked on activity start
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchInput = query;
-                return true;
+                return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 searchInput = newText;
-                return true;
+                return false;
             }
         });
 
         btn.setOnClickListener((e) -> makeRestRequest(searchInput));
 
-        this.adapter = new WebsiteAdapter(this, websiteList);
-        this.lv = (ListView) this.findViewById(R.id.listView);
-        assert lv != null;
-        lv.setAdapter(adapter);
+    }
 
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private void makeRestRequest(String searchInput) {
+
+        hideKeyboard(this);
+
         assert searchInput != null;
         queue = Volley.newRequestQueue(this);
+        adapter.clear();
 
         // JSon object request
         JSONObject ob = new JSONObject();
@@ -128,8 +145,8 @@ public class SearchActivity extends AppCompatActivity {
                         JSONArray site = response.getJSONArray("results"); // always the first response
                         for (int i = 0; i < site.length(); i++) {
                            JSONObject siteObj = (JSONObject) site.get(i);
-                       Website website = new Website(siteObj.getString("title"),siteObj.getString("url"),siteObj.getString("baseUrl"));
-                       websiteList.add(website);
+                           Website website = new Website(siteObj.getString("title"),siteObj.getString("url"),siteObj.getString("baseUrl"));
+                           adapter.add(website);
                         }
 
                     } catch (JSONException e) {
