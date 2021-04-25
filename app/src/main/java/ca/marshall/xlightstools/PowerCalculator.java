e129eeb1cf7554144f5a3744a6638c104cfd2fa1
Mathew <mathew.r.erwin@gmail.com>
2021-04-25 19:57:19 -0400

    Added updated power tools backend started props organizer

--- app/src/main/java/ca/marshall/xlightstools/PowerCalculator.java
+++ app/src/main/java/ca/marshall/xlightstools/PowerCalculator.java
@@ -3,8 +3,16 @@ package ca.marshall.xlightstools;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PowerCalculator extends AppCompatActivity {
+
+    EditText NumOfStrings = (EditText) findViewById(R.id.editText6);
+    EditText LightsPerString = (EditText) findViewById(R.id.editText3);
+    EditText PowerSupply = (EditText) findViewById(R.id.botdiin);
+    EditText MaxUsage = (EditText) findViewById(R.id.circov);
+    EditText Voltage = (EditText) findViewById(R.id.sttl);
+    EditText AmpsPerLight = (EditText) findViewById(R.id.sabl);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
